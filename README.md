32b71ae6cc26d3c672c1396c7969709bc93bc3d7
Jake Johnson <thejakej@umich.edu>
2016-05-10 10:27:30 -0400

    first revision

---
+++ README.md
@@ -0,0 +1,6 @@
+Fresh Tomatoes Readme
+
+Make sure fresh_tomatoes.py, media.py and entertainment_center.py
+are all in the same folder
+
+Run entertainment_center.py to open movie website
--- media.py
+++ media.py
@@ -1,11 +1,16 @@
+# define Movie constructor
+# define show_trailer function
+
 import webbrowser
 
 class Movie():
+	# initialize member variables
 	def __init__(self, title_in, story_in, poster_in, trailer_in):
 		self.title = title_in
 		self.storyline = story_in
 		self.poster_image_url = poster_in
 		self.trailer_youtube_url = trailer_in
 
+	# displays movie trailer
 	def show_trailer(self):
 		webbrowser.open(self.trailer)
